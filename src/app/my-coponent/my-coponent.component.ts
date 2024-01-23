8d8701cd45501f4ae7a96f1b822139ff4a0d63a0
erickterra <78423486+terrasoftworks@users.noreply.github.com>
2024-01-23 10:28:25 -0300

    Projeto Treinamento

---
+++ src/app/my-coponent/my-coponent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-coponent',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-coponent.component.html',
+  styleUrl: './my-coponent.component.css'
+})
+export class MyCoponentComponent {
+
+}
