517bd01acfdcc3970dc637573ad8196388bf3e88
erickterra <erick.terra@papersolutions.com.br>
2024-01-25 10:22:36 -0300

    Ps Table e gitignore atualizado

--- Frontend/src/app/models/user.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-    id?: number,
-    nome: string,
-    login: string,
-    senha: string, 
-    criado: Date,
-    alterado: Date
-}
--- Frontend/src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    api: 'http://localhost:5109', 
-    version: 'v1'
-};
