b34c331d31fe3d20e9c596c44488f69dcd74ac31
erickterra <erick.terra@papersolutions.com.br>
2024-01-25 16:58:25 -0300

    Toolbox, Navbar, Home page melhorada

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet, RouterLink, HttpClientModule, MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Frontend/src/app/pages/home/home.component.ts
+++ Frontend/src/app/pages/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, MatButton, MatIcon],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
