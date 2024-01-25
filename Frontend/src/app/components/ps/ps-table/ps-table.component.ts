32222a18e775b06684d29f85c3da168fb1d4ad99
erickterra <erick.terra@papersolutions.com.br>
2024-01-25 16:59:15 -0300

    ps-table melhorada

--- Frontend/src/app/components/ps/ps-table/ps-table.component.ts
+++ Frontend/src/app/components/ps/ps-table/ps-table.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { User } from '../../../models/user';
 import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-ps-table',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [MatTableModule, MatCardModule],
   templateUrl: './ps-table.component.html',
   styleUrl: './ps-table.component.css'
 })
 export class PsTableComponent {
   @Input() data: User[] = [];
+  @Input() title = '';
   columnsToDisplay: string[] = ['id', 'name', 'login', 'created','modified'];
 }
