07c41454df6aeb0eb48803d89f936f6f5928cdb4
erickterra <erick.terra@papersolutions.com.br>
2024-02-07 18:21:13 -0300

    Adicionado Autenticação via Azure

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -7,14 +7,76 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { FormsModule } from '@angular/forms';
+import { MSAL_INSTANCE, MsalModule } from '@azure/msal-angular';
+import { AuthenticationResult, IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
+import { MsalService } from '@azure/msal-angular';
+import { CommonModule } from '@angular/common';
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: '071b369a-b18e-48e6-b027-ad81c1283939',
+      redirectUri: 'http://localhost:4200'
+    }
+  });
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HttpClientModule, MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule, MatSidenavModule, FormsModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, HttpClientModule, MatToolbarModule, 
+    MatIconModule, MatButtonModule, MatCardModule, MatSidenavModule, FormsModule,
+    MsalModule
+  ],
+  providers: [
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    MsalService
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit{
+  username?: string = '';
+
+  constructor(private authService: MsalService) {
+
+  }
+  ngOnInit(): void {
+    this.initialize();
+    if(this.authService.instance.getActiveAccount() == null)
+    {
+      this.login();
+    }
+    else
+    {
+      this.username = this.authService.instance.getActiveAccount()?.username
+    }
+  }
+
+  async initialize()
+  {
+    await this.authService.instance.initialize();
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.instance.getActiveAccount() != null
+  }
+
+  login() {
+    this.authService.loginPopup()
+      .subscribe((response: AuthenticationResult) => {
+        this.authService.instance.setActiveAccount(response.account);
+        this.username = response.account.username;
+      });
+  }
+
+  logout() {
+    this.authService.logout()
+  }
+
 }
+
+
