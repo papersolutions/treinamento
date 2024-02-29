f7e5e00b1507e9bb39d124634e65df3ed5576d8f
flavia-paper <flavia.schnaider@papersolutions.com.br>
2024-02-29 11:58:41 -0300

    Adicionei icones de Actions na Tabela

--- Frontend/src/app/components/ps/ps-table/ps-table.component.ts
+++ Frontend/src/app/components/ps/ps-table/ps-table.component.ts
@@ -2,12 +2,16 @@ import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
-import { PageEvent, MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationDialogComponent } from '../../../confirmation-dialog/confirmation-dialog.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-ps-table',
   standalone: true,
-  imports: [MatTableModule, MatCardModule, CommonModule, MatPaginatorModule],
+  imports: [MatTableModule, MatCardModule, CommonModule, MatPaginatorModule, MatIconModule, MatButtonModule],
   templateUrl: './ps-table.component.html',
   styleUrl: './ps-table.component.css'
 })
@@ -16,5 +20,17 @@ export class PsTableComponent {
   @Input() title = '';
   @Input() columnsToDisplay: string[] = [];
 
+  constructor(private dialog: MatDialog) { }
 
+  openConfirmationDialog(element: any): void {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '250px',
+      data: { message: 'Are You Sure?' }
+    })
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('Deleting item:', element);
+      }
+    });
+  }
 }
